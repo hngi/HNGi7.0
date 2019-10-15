79cc5ee1295b04b4b2af0cdaf0f7d58ef6b0a42d
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2019-10-15 14:34:37 +0100

    config.php file for backend

---
+++ config/database.php
@@ -0,0 +1,19 @@
+<?php
+ session_start();
+
+    class DB {
+        public function __construct(){
+            require "constants.php";
+        }
+        public function get_connection(){
+            $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            if(mysqli_connect_errno()){
+                echo "Database connection failed with following errors:" . mysqli_connect_error();
+                die();
+            }else{
+                return $db;
+            }
+        }
+    }
+?>
+
