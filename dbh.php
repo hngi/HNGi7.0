9b2ce6dc068ce3870a0118bcb334e1ee1c7eb6d6
odun <biodun.agbolade@gmail.com>
2019-10-16 09:05:56 +0100

    Admin to View Registered Interns (Backend)

--- dbh.php
+++ dbh.php
@@ -11,7 +11,14 @@ class Database
       {
         	$dbh = mysqli_connect($this->host,$this->user,$this->pass,$this->db);
         	return $dbh;
+        }
+           function saveRecords()
+      	{
+          	$dbhh = $this->connect();
+            $result = mysqli_query($dbhh, "SELECT * FROM interns");
+          return $result;
     	}
 
-    }
+
+  }
 ?>
