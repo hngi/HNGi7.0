55ba3f060993dcd5f4df5dfa53481e6e47344750
Ayenko4all <kenny4all40@gmail.com>
2019-10-16 06:36:19 +0100

    contactform

---
+++ classControllers/adminClass.php
@@ -0,0 +1,20 @@
+<?php
+//alway call global $database inside you method
+class AdminClass{
+  
+
+
+  /**************METHOD TO HANDLE SENDING OF CONTACT FORM****************/
+  public function contactFormMailer($name,$email,$subject,$message){
+
+    global $database;
+    
+   
+    
+
+
+  }
+
+
+}
+
