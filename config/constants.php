3984babbc4eb02bd0dd93d4d390a378083455cc7
Olatunji <sirhollyfat@gmail.com>
2019-10-17 08:53:19 +0100

    Setup local testing environment

--- config/constants.php
+++ config/constants.php
@@ -1,7 +1,19 @@
 <?php
-    define('DB_HOST', 'remotemysql.com');
-    define('DB_USER', 'XnTQPp4tAK');
-    define('DB_PASS', 'wqmtOm6brY');
-    define('DB_NAME', 'XnTQPp4tAK');
+
+    define('ENV','local');
+
+    if(defined('ENV') == "production") {
+        define('DB_HOST', 'remotemysql.com');
+        define('DB_USER', 'XnTQPp4tAK');
+        define('DB_PASS', 'wqmtOm6brY');
+        define('DB_NAME', 'XnTQPp4tAK');
+    }elseif (defined('ENV') == "local"){
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'root');
+        define('DB_PASS', '');
+        define('DB_NAME', 'hngi7');
+    }
+
+
 ?>
 
