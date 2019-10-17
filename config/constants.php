5cd5f39aba05f00aff4a8565b0bf82ab198e4c23
Olatunji <sirhollyfat@gmail.com>
2019-10-17 09:16:53 +0100

    Remote mysql setup with dummy data

--- config/constants.php
+++ config/constants.php
@@ -1,23 +1,8 @@
 <?php
-
-    define('ENV','local');
-
-    /*if(defined('ENV') == "production") {
-        define('DB_HOST', 'remotemysql.com');
-        define('DB_USER', 'XnTQPp4tAK');
-        define('DB_PASS', 'wqmtOm6brY');
-        define('DB_NAME', 'XnTQPp4tAK');
-    }elseif (defined('ENV') == "local"){
-        define('DB_HOST', 'localhost');
-        define('DB_USER', 'root');
-        define('DB_PASS', '');
-        define('DB_NAME', 'hngi7');
-    }*/
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'hngi7');
+    define('DB_HOST', 'remotemysql.com');
+    define('DB_USER', 'XnTQPp4tAK');
+    define('DB_PASS', 'wqmtOm6brY');
+    define('DB_NAME', 'XnTQPp4tAK');
 
 
 ?>
--- login.php
+++ login.php
@@ -5,3 +5,73 @@
  * Date: 10/17/2019
  * Time: 9:12 AM
  */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Team Icon</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-default" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Team Icon</a>
+    </div>
+
+    </div><!-- /.navbar-collapse -->
+</nav>
+
+
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="panel panel-success">
+                    <div class="panel-heading">Account Login</div>
+                    <div class="panel-body">
+                        <form action="" method="post">
+                            <?php
+                            if(isset($_SESSION['err_msg'])) {
+                                echo $_SESSION['err_msg'];
+                                unset($_SESSION['err_msg']);
+                            }
+                            ?>
+                            <div class="form-group">
+                                <label for="email">Email Address</label>
+                                <input type="email" id="email" name="email" class="form-control" required placeholder="Email Address">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" required class="form-control" id="password" placeholder="Password">
+                            </div>
+
+
+                            <div class="form-group">
+                                <input type="submit" name="register" class="btn btn-success" value="Login">
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
