1b10721eb1ec0c1db128a63a97beb952ffc7bef2
Emmanuel Owojori <34447078+phiiileo@users.noreply.github.com>
2019-10-18 11:37:36 +0100

    Revert "Update on the Component for the header. Frontend"

--- js/app.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var includes = $('[data-include]');
-    jQuery.each(includes, function() {
-        var file = './components/' + $(this).data('include') + '.html';
-        $(this).load(file);
-    });
-
-});
