647d5897cf7fc8dcbddf374e5f7307403b9602c1
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2019-10-19 11:57:38 +0100

    modified

--- index.php
+++ index.php
@@ -23,27 +23,7 @@
         <?php
           include('fragments/site_header.php');
         ?>
-        <!--header id="header">
-            <nav>
-                <div class="logo-container">
-                    <img src="https://bit.ly/2M2pUlV" alt="HNG Internship Logo" id="logo">
-                </div>
-                <div class="hamburger-container">
-                    <img src="https://bit.ly/2OHfmKZ" id="hamburger" alt="" width="35" height="35">
-                </div>
-                <ul class="nav-ul desktop-only">
-                    <li class="nav-link-1"><a href="index.html">Home</a></li>
-                    <li class="nav-link-1"><a href="hng6.html">HNG6</a></li>
-                    <li class="nav-link-1"><a href="mentorpage.html">Mentors</a></li>
-                    <li class="nav-link-1"><a href="contactform.html">Contact</a></li>
-                    <li class="nav-link-1"><button class="cta-button button--midBlue" id="join-hng"><a
-                                href="join-intern.html">Join
-                                HNG</a></button></li>
-                </ul>
-            </nav>
-        </header-->
-
-        <main>
+         <main>
             <div class="main-first-row">
                 <div class="welcome-column">
                     <h1 id="welcome-text" class="text__dark-blue">
@@ -54,7 +34,7 @@
                         the bridge between learning to code, and becoming the best in the world.
                     </p>
                     <button class="cta-button button--midBlue" id="buttt"> 
-                        <a href="join-intern.html" id="butt">Get Started</a>
+                        <a href="join-intern.php" id="butt">Get Started</a>
                     </button>
                 </div>
                 <div class="image-column desktop-only">
@@ -86,84 +66,7 @@
                 </div>
 
             </div>
-<!--
-    <script>
-    //darkangel cc.
-    const daysElem = document.querySelector('#days'), //Edit this selector to pick the days element in your HTML.
-    hoursElem = document.querySelector('#hours'),
-    minutesElem = document.querySelector('#minutes'),
-    secondsElem = document.querySelector('#seconds'),
-    countDownWrapper = document.querySelector('.countdown'); //This is the counter that wraps all 4 numbers.
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2020 22:0:0").getTime();
-
-// Update the count down every 1 second
-var counter = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutesLeft = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the results.
-    daysElem.innerText = daysLeft;
-    hoursElem.innerText = hoursLeft;
-    minutesElem.innerText = minutesLeft;
-    secondsElem.innerText = secondsLeft;
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(counter);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-    </script>
--->
-
-<script>
-        //darkangel cc.
-        const daysElem = document.querySelector('#days'), //Edit this selector to pick the days element in your HTML.
-    hoursElem = document.querySelector('#hours'),
-    minutesElem = document.querySelector('#minutes'),
-    secondsElem = document.querySelector('#seconds'),
-    countDownWrapper = document.querySelector('.countdown'); //This is the counter that wraps all 4 numbers.
-
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 1, 2020 22:0:0").getTime();
-
-function updateClock() {
-  // Get today's date and time
-  var now = new Date().getTime();
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-  // Time calculations for days, hours, minutes and seconds
-  var daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutesLeft = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
-    // Display the results.
-    daysElem.innerText = daysLeft;
-    hoursElem.innerText = hoursLeft;
-    minutesElem.innerText = minutesLeft;
-    secondsElem.innerText = secondsLeft;
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    document.getElementById("demo").innerHTML = "EXPIRED";
-    return;
-  }
-  setTimeout(updateClock, 1000);
-}
-updateClock();
-</script>
             <div class="main-third-row text_center">
                 <div class="">
                     <h3 class="subheading text__dark-blue" style = "font-weight: bold">
@@ -675,96 +578,16 @@ updateClock();
         </div>
         <nav>
             <ul>
-<<<<<<< HEAD:index.php
-                <li class="nav-link-1-vertical"><a href="index.php">Home</a></li>
-                <li class="nav-link-1-vertical"><a href="hng6.php">HNGi6</a></li>
-                <li class="nav-link-1-vertical"><a href="mentorpage.php">Mentors</a></li>
-                <li class="nav-link-1-vertical"><a href="contactform.php">Contact</a></li>
-                <li class="nav-link-1-vertical"><a href="join-intern.php" class="text__dark-blue">Join HNGi</a></li>
-=======
                 <li class="nav-link-1-vertical"><a href="index.html">Home</a></li>
                 <li class="nav-link-1-vertical"><a href="hng6.html">HNGi6</a></li>
                 <li class="nav-link-1-vertical"><a href="mentorpage.html">Mentors</a></li>
                 <li class="nav-link-1-vertical"><a href="contactform.html">Contact</a></li>
                 <li class="nav-link-1-vertical"><a href="join-intern.html" class="text__dark-blue">Join HNGi</a></li>
->>>>>>> af688b8bf4a9fe94346e55aa35004eb02e949ea2:index.html
 
             </ul>
         </nav>
     </aside>
 
-<script>
-    const hamburger = document.getElementById("hamburger");
-    const aside = document.getElementsByTagName("aside")[0];
-    const hide_aside = document.getElementsByClassName("hide-aside")[0];
-    let mediaQuery = window.matchMedia("(max-width: 960px)");
-    let slideIndex = 0;
-
-    showSlides(mediaQuery);
-    mediaQuery.addListener(showSlides);
-
-
-    //const cards = document.getElementsByClassName("card1");
-    function showSlides(mediaQueryx) {
-        let i;
-
-        let cards = document.getElementsByClassName("card1");
-        if (mediaQuery.matches) {
-
-            //var dots = document.getElementsByClassName("dot");
-            for (i = 0; i < cards.length; i++) {
-                cards[i].style.display = "none";
-            }
-            slideIndex++;
-            if (slideIndex > cards.length) { slideIndex = 1 }
-
-            cards[slideIndex - 1].style.display = "block";
-            //dots[slideIndex - 1].className += " active";
-            setTimeout(showSlides, 2000);// Change image every 2 seconds*/
-
-        }
-        else {
-            for (i = 0; i < cards.length; i++) {
-                cards[i].style.display = "block";
-            }
-
-        }
-
-
-    }
-    //handle sroll to top arrow
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { 
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); 
-            $target.focus(); 
-          };
-        });
-      }
-    }
-  });
-
-</script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
     crossorigin="anonymous"></script>
@@ -800,6 +623,105 @@ $('a[href*="#"]')
         }
         window.addEventListener('scroll', (event) => displayArrow(event))
     </script>
+    <script>
+    const hamburger = document.getElementById("hamburger");
+    const aside = document.getElementsByTagName("aside")[0];
+    const hide_aside = document.getElementsByClassName("hide-aside")[0];
+    let mediaQuery = window.matchMedia("(max-width: 960px)");
+    let slideIndex = 0;
+
+    showSlides(mediaQuery);
+    mediaQuery.addListener(showSlides);
+
+
+    //const cards = document.getElementsByClassName("card1");
+    function showSlides(mediaQueryx) {
+        let i;
+
+        let cards = document.getElementsByClassName("card1");
+        if (mediaQuery.matches) {
+
+            //var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < cards.length; i++) {
+                cards[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > cards.length) { slideIndex = 1 }
+
+                cards[i].style.display = "block";
+            }
+
+        }
+
+    }
+    //handle sroll to top arrow
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.focus(); 
+          };
+        });
+      }
+    }
+  });
+
+</script>
+<script>
+        //darkangel cc.
+        const daysElem = document.querySelector('#days'), //Edit this selector to pick the days element in your HTML.
+    hoursElem = document.querySelector('#hours'),
+    minutesElem = document.querySelector('#minutes'),
+    secondsElem = document.querySelector('#seconds'),
+    countDownWrapper = document.querySelector('.countdown'); //This is the counter that wraps all 4 numbers.
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2020 22:0:0").getTime();
+
+function updateClock() {
+  // Get today's date and time
+  var now = new Date().getTime();
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+  // Time calculations for days, hours, minutes and seconds
+  var daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hoursLeft = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutesLeft = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
+    // Display the results.
+    daysElem.innerText = daysLeft;
+    hoursElem.innerText = hoursLeft;
+    minutesElem.innerText = minutesLeft;
+    secondsElem.innerText = secondsLeft;
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    document.getElementById("demo").innerHTML = "EXPIRED";
+    return;
+  }
+  setTimeout(updateClock, 1000);
+}
+updateClock();
+</script>
 </body>
 
 </html>
