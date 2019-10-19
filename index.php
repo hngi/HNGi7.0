a7f224a9f405ac084819d77e2ab61cde2ee0114b
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-19 14:53:27 +0100

    file pulled from upstream

--- index.php
+++ index.php
@@ -848,43 +848,6 @@ $('a[href*="#"]')
   });
 
 </script>
-<<<<<<< HEAD
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"
-    crossorigin="anonymous"></script>
-    <script>
-        let scrollArrow = document.querySelector('#scroll-down-arrow');
-        function scrollWin() {
-            window.scrollBy(0, 500);
-            console.log(window.scrollY)
-            if (window.scrollY >= 5000) {
-                scrollArrow.style.display = "none";
-            }
-        }
-        const hideArrow = (event) => {
-            if (window.scrollY >= 5000) {
-                scrollArrow.style.display = "none";
-            }
-        }
-        window.addEventListener('scroll', (event) => hideArrow(event))
-    
-        const displayArrow = (event) => {
-            if (window.scrollY < 5000) {
-                scrollArrow.classList.add('bounceInDown')
-                scrollArrow.style.display = "block";
-            }
-        }
-        window.addEventListener('scroll', (event) => displayArrow(event))
-    </script>
-=======
 <script>
         //darkangel cc.
         const daysElem = document.querySelector('#days'), //Edit this selector to pick the days element in your HTML.
@@ -920,8 +883,6 @@ function updateClock() {
 }
 updateClock();
 </script>
->>>>>>> ad7c6df42595e5f10038457dd19a1e16e0a4ead2
 </body>
 
 </html>
-
