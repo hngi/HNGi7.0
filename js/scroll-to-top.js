a44ed92cf72a49bb5eb5d6f9d49e79e29826173c
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2019-10-19 20:24:25 +0100

    Remove console.log)

--- js/scroll-to-top.js
+++ js/scroll-to-top.js
@@ -10,18 +10,11 @@
     return ( (rootNode[sh] || body[sh]) - rootNode.clientHeight - (rootNode[st] || body[st]) )
   }
 
-  function test() {
-    console.log("Scroll Remaining", getScrollRemaining());
-    setTimeout(test, 1000);
-  }
-
   window.addEventListener('scroll', (e) => {
     if (getScrollRemaining() < 60) {
       backToTop.style.display = 'block';
     } else {
       backToTop.style.display = 'none';
     }
-  })
-
-  test();
+  });
 })();
