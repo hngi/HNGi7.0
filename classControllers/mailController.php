8108d52713d289860f554aae47a5360aafa526e5
iPeluwa <Ipeluwa@gmail.com>
2019-10-23 22:01:18 +0100

    Update mailController.php

--- classControllers/mailController.php
+++ classControllers/mailController.php
@@ -263,17 +263,18 @@ function forGetPasswordMail($url,$subject,$email,$fullname)
     <td style='padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;'>
                       
                        <p>You recently requested to reset your password for your HNGi account.<br>
-                        Use the button below to reset it.<br>
+                        Use the link below to reset it.<br>
                         This password reset is only valid for the next 24 hours.
                         Reset your password $fullname<br><br></p>
 
-                       <p>For security, If you did not request a password reset, please ignore this email or contact support ( http://hngi7.hng.tech/contactform.php ) if you have questions.</p>
+                       <p>For security, If you did not request a password reset, please ignore this email or <a href='http://hngi7.hng.tech/contactform.php'> Contact support</a> if you have questions.</p>
 
+                       <p>If you’re having trouble clicking the link above, copy and paste the URL below into your web browser.
+
+                       <br> <a href='http://hngi7.hng.tech/admin_login.php'>Follow this link to login</a></p>
                      <p> Thanks,<br>
                       The User Suppport Team.</p>
 
-                      <p>If you’re having trouble clicking the link above, copy and paste the URL below into your web browser.
-                      $fullname <br> <a href='http://hngi7.hng.tech/admin_login.php'>Follow this link to login</a></p>
                       </td>
                   </tr>
 <tr>
