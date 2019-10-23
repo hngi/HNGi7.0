f0421846142d9f1d76194f9a1d1eefeac84f89b4
iPeluwa <Ipeluwa@gmail.com>
2019-10-23 21:08:59 +0100

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -49,7 +49,7 @@ if (isset($_POST['contact-btn'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>FAQ2</title>
+  <title>Contact Us</title>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
