627737313e0e487cdaf9ae1bff5fd0662a66b418
Olatunji <sirhollyfat@gmail.com>
2019-10-29 10:42:43 +0100

    Return error message if subscriber already exist!

--- subcribe_ajax.php
+++ subcribe_ajax.php
@@ -5,3 +5,20 @@
  * Date: 10/29/2019
  * Time: 10:40 AM
  */
+
+require_once "classControllers/init.php";
+
+$subscriber = new Subscribers();
+
+if(isset($_POST['add'])){
+
+    $output = array();
+    $email = $database->escape_string($_POST['email']);
+
+    if($subscriber->subscriberExists($email)){
+        $output['status'] = false;
+        $output['message'] = "You have already subscribe to mailing list!";
+    }else{
+
+    }
+}
