fd76ec8de5e91893e5d50e6db7c6ede3c06e15f2
Olatunji <sirhollyfat@gmail.com>
2019-10-29 10:31:52 +0100

    Subscriber class created successfully

---
+++ classControllers/Subscribers.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hollyphat
+ * Date: 10/29/2019
+ * Time: 10:31 AM
+ */
+
+class Subscribers
+{
+
+}
--- fragments/subscribe.php
+++ fragments/subscribe.php
@@ -7,43 +7,42 @@
  */
 
 ?>
+
+
 <a class="btn btn-primary" data-toggle="modal" href="#modal-subscribe">Subscribe</a>
 
 <div class="modal fade" id="modal-subscribe">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title">Modal title</h4>
+                <h4 class="modal-title">Subscribe Now to get latest update</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
             </div>
             <div class="modal-body">
                 <section class="one">
-                    <div class="logo">
-                        <img src="https://image.flaticon.com/icons/svg/143/143361.svg">
+                    <div class="logos text-center">
+                        <img src="https://image.flaticon.com/icons/svg/143/143361.svg" class="img-responsive" width="200">
                     </div>
-                    <h2 class="heading">
-                        please subscribe to get updates
+                    <h2 class="heading text-center">
+                        Please subscribe to get updates
                     </h2>
-                    <p> signup with your email to get latest updates
                     </p>
-                    <form>
-                        <input type='text' placeholder="enter your email"><br/>
-                        <button class="btn" role="button">
-                            subscribe
-                        </button>
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <input type='email' class="form-control" required placeholder="enter your email" id="subscribe-form">
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-info" role="button">
+                                <i class="fa fa-check"></i> Subscribe
+                            </button>
+                        </div>
                     </form>
                 </section>
-                <section class="two">
-                    <h3>
-                        thank you for subscribing !
-                    </h3>
-                    <div class="close">
-                    </div>
-                </section>
+                <section id="subscribe-feedback"></section>
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
