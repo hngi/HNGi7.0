6cd1d30139dbd21993ef4b663ceb086ae2c43eba
Olatunji <sirhollyfat@gmail.com>
2019-10-29 09:56:27 +0100

    Create modal for subscription

--- fragments/subscribe.php
+++ fragments/subscribe.php
@@ -5,3 +5,25 @@
  * Date: 10/29/2019
  * Time: 9:53 AM
  */
+
+?>
+<a href="">Subscribe</a>
+
+<a class="btn btn-primary" data-toggle="modal" href="#modal-id">Trigger modal</a>
+<div class="modal fade" id="modal-id">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                Modal body ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
