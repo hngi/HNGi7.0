9379911013f1fe49bb3775067ab680a9c2f44ee8
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-30 13:17:26 +0100

    mentorpage bugs fixed

--- js/mentorSetupPage.js
+++ js/mentorSetupPage.js
@@ -1,40 +1,50 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-          form.classList.add('was-validated');
-        }, false);
-      });
-    }, false);
-  })();
+(function () {
+ 'use strict';
+ window.addEventListener('load', function () {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.getElementsByClassName('needs-validation');
+  // Loop over them and prevent submission
+  var validation = Array.prototype.filter.call(forms, function (form) {
+   form.addEventListener('submit', function (event) {
+    if (form.checkValidity() === false) {
+     event.preventDefault();
+     event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+   }, false);
+  });
+ }, false);
+})();
 
-  //For the accordion in the area of expertise section
-  const wrap = document.querySelector('#wrap')
-      
-      wrap.addEventListener('click', (e)=>{
-        
-          if(e.target.classList.contains('accordion-label')){
-              content = e.target.nextElementSibling;              
-              if(!e.target.classList.contains('active')){
-                  const findActiveButton = document.querySelector('.active')
-                  if(findActiveButton){
-                      findActiveButton.classList.remove('active')
-                      findActiveButton.nextElementSibling.style.maxHeight = 0;
-              }
-                  e.target.classList.add('active')
-                  content.style.maxHeight = content.scrollHeight + "px"
-              }else{
-                  e.target.classList.remove('active')
-                  content.style.maxHeight = 0;
-              }
-          }
-      })
+//For the accordion in the area of expertise section
+const wrap = document.querySelector('#wrap')
+
+wrap.addEventListener('click', (e) => {
+ if (e.target.classList.contains('fa-angle-down')) {
+  content = e.target.nextElementSibling;
+  // console.log(content);
+  // if (!e.target.classList.contains('active')) {
+  //  const findActiveButton = document.querySelector('.active')
+  //  if (findActiveButton) {
+  //   findActiveButton.classList.remove('active')
+  //   findActiveButton.nextElementSibling.style.maxHeight = 0;
+  //  }
+  //  e.target.classList.add('active')
+  //  content.style.maxHeight = content.scrollHeight + "px"
+  // } else {
+  //  e.target.classList.remove('active')
+  //  content.style.maxHeight = 0;
+  // }
+  e.target.classList.add('fa-angle-up');
+  e.target.classList.remove('fa-angle-down');
+  content.style.maxHeight = content.scrollHeight + "px";
+  
+ }else {
+  if (e.target.classList.contains('fa-angle-up')) {
+   e.target.classList.add('fa-angle-down');
+   e.target.classList.remove('fa-angle-up');
+   content.style.maxHeight = 0;
+  }
+ }
+})
