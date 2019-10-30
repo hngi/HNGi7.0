3dce9349a0d5e14a70d2ad8aa3df00035023e1e4
Olatunji <sirhollyfat@gmail.com>
2019-10-30 14:30:53 +0100

    Bootstrap js added to subscription

--- fragments/subscribe.php
+++ fragments/subscribe.php
@@ -9,6 +9,7 @@
 ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
 <a class="btn btn-primary" data-toggle="modal" href="#modal-subscribe">Subscribe</a>
 
