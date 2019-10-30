330afbcf85d58ca67a6ef315457a66739aae93ac
Olatunji <sirhollyfat@gmail.com>
2019-10-30 14:30:24 +0100

    Subscription Jquery version changed from slim to full version

--- fragments/subscribe.php
+++ fragments/subscribe.php
@@ -8,9 +8,7 @@
 
 ?>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 <a class="btn btn-primary" data-toggle="modal" href="#modal-subscribe">Subscribe</a>
 
