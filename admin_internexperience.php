49ece5d953892c9850bcd2b273c30bd08ad990a8
phiiileo <emmanuelboluwajib1@gmail.com>
2019-10-31 18:07:01 +0100

    Relinked the new intern experience

--- admin_internexperience.php
+++ admin_internexperience.php
@@ -55,7 +55,6 @@ $all_experiences = $internExperience->fetch_experiences();
 </head>
 
 <body>
- <section class="container-fluid">
   <section class="jumbo">
    <?php include('fragments/site_header.php'); ?>
    <h2>Admin Intern Experience Page</h2>
@@ -107,7 +106,6 @@ $all_experiences = $internExperience->fetch_experiences();
    <?php include('fragments/site_footer.php'); ?>
 
   </main>
- </section>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
