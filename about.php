ffae45b7f519f0c11cc1fa4d38915e367953914c
Lateef Taofeek <52785989+zonebyte@users.noreply.github.com>
2019-11-02 12:45:09 +0100

    Add files via upload

--- about.php
+++ about.php
@@ -12,8 +12,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="css/header-footer.css">    
     <link rel="stylesheet" href="css/about.css">
+    <link rel="icon" type="img/png" href="images/hng-favicon.png">
 </head>
 <body>
         <section class="container-fluid">
