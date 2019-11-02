c359d87390445e9e3fe06dabefea8e43841aef8f
papa28x4 <swissfair@yahoo.co.uk>
2019-11-02 13:19:46 +0100

    refresh

--- registered_sponsors.php
+++ registered_sponsors.php
@@ -90,84 +90,6 @@ $data = $sponsors->getAllSponsor();
                             <a href="#" onclick="javascript:printDiv('printablediv')">
                                 <button type="button" class="btn btn-primary btn-sm" id="export">Export to PDF</button>
                             </a> -->
-<<<<<<< HEAD
-                        </div>
-                        <!-- <div id="printablediv" class="table-responsive"> -->
-                        <div id="printablediv" class="sponsors">
-                            <div class="scroll">
-                            <!-- <table id="my-table" class="table table-hover table-bordered mt-3 mb-1"> -->
-                            <table id="my-table" class="table table-hover sponsor-table">
-                                <!-- <thead class="table-primary"> -->
-                                <thead>
-                                    <tr>
-                                    <th data-heading="sn">SN<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="expertise">Area Of Expertise<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="photo">Photo<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="name">Name<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="email">Email<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="phone">Phone<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <!-- <th>Link To Linkedin</th> -->
-                                    <th data-heading="cv">Link To Cv<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="Interest">Why Interested<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="state">Current State<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="employment-status">Employment Status<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                    <th data-heading="timeStamp">Timestamp<!--<i class="fas fa-sort-up"></i><i class="fas fa-sort-down"></i>--></th>
-                                        <th>Action</th>
-
-
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                        <td>1</td>
-                        <td> Marketing Officer </td>
-                        <td><img src="uploads/5db802bf1a6d53.33443776.jpg" style="width: 50px; height: 50px;"></td>
-                        <td> Joel</td>
-                        <td>Joelokon123@yahoo.com</td>
-                        <td>08153471606</td>
-                        <td>http://www.linkedin.com</td>
-                        <td>i want to support</td>
-                        <td>Lagos</td>
-                        <td>employed</td>
-                        <td>2019-10-29 09:13:35</td>
-                        <td><a href="delete_mentor.php?deleteMentorId=31" class="btn btn-danger btn-sm">Delete</a></td>
-                        
-
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td> CEO techBAM </td>
-                        <td><img src="uploads/5db802bf1a6d53.33443776.jpg" style="width: 50px; height: 50px;"></td>
-                        <td>Victor</td>
-                        <td>victorade@yahoo.com</td>
-                        <td>0815897536</td>
-                        <td>http://www.linkedin.com</td>
-                        <td>i want to support</td>
-                        <td>Abuja</td>
-                        <td>employed</td>
-                        <td>2019-10-29 09:13:45</td>
-                        <td><a href="delete_mentor.php?deleteMentorId=31" class="btn btn-danger btn-sm">Delete</a></td>
-                        
-
-                    </tr>
-
-                                </tbody>
-                            </table>
-                          </div>
-                        </div>
-                    <?php
-                    }
-                    ?>
-                    <div class="buttonDiv">
-                                <!--<a href="exports/export-to-pdf-sponsors.php">-->
-                                <a href="#" onclick="javascript:printDiv('printablediv')">
-                                    <button type="button" class="exports" id="export">Print</button>
-                                </a>
-                                <a href="#" >
-                                    <button type="button" class="exports" id="exportAs">Export</button>
-                                </a>
-                        </div>            
-=======
                 </div>
                 <!-- <div id="printablediv" class="table-responsive"> -->
                 <div id="printablediv">
@@ -204,7 +126,6 @@ $data = $sponsors->getAllSponsor();
                             </tbody>
                         </table>
                     </div>
->>>>>>> dfd4bf4ffac8d91277286a8549a9cdf0a994fca3
                 </div>
             <?php
             }
@@ -233,27 +154,6 @@ $data = $sponsors->getAllSponsor();
                 </div>
             </div>
             <div id="centralize">
-<<<<<<< HEAD
-			<h2>Sponsor Details</h2>
-			<em id="no-intern">No sponsor selected</em>
-            <br />
-            <p class="details" style="margin-left:10%;"><span id="photo"></span></p>
-			<p class="details">Name: <span id="name"></span></p>
-			<p class="details">Email: <span id="email"></span></p>
-			<p class="details">Phone Number: <span id="phone"></span></p>
-			<p class="details">Expertise: <span id="expertise"></span></p>
-			<p class="details">CV link: <span id="cv"></span></p>
-            <p class="details">State of residence: <span id="state"></span></p>
-            <p class="details">Employment Status: <span id="employment-status"></span></p>
-            <p class="details">Why Interested: <span id="Interest"></span></p>
-            <p class="details">Timestamp: <span id="timeStamp"></span></p>
-            <!-- <div href="" id="details-return">Back to Overview</div> -->
-            <div id="navigator">
-                <i class="fas fa-chevron-left fa-2x left navigator"></i> 
-                <p class="details"><span id="sn"></span></p>
-                <i class="fas fa-chevron-right fa-2x right navigator"></i>
-            </div>
-=======
                 <h2>sponsor Details</h2>
                 <em id="no-intern">No sponsor selected</em>
                 <br />
@@ -273,7 +173,6 @@ $data = $sponsors->getAllSponsor();
                     <p class="details"><span id="sn"></span></p>
                     <i class="fas fa-chevron-right fa-2x right navigator"></i>
                 </div>
->>>>>>> dfd4bf4ffac8d91277286a8549a9cdf0a994fca3
             </div>
         </section>
     </main>
