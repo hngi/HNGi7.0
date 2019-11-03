5c6f884da9d2ad393fe1c1b308dc66ac0addb60d
Eniola Olowoyo <olowoyoeniola@gmail.com>
2019-11-03 16:40:39 +0100

    join-intern and faq nav fix

--- intern-experience.php
+++ intern-experience.php
@@ -61,13 +61,13 @@
         h2.heading {
           color: #084482;
           font-weight: bolder;
-          padding: 0 1em;
+          padding: 0 4px;
         }
         p.para {
           width: 100%;
           text-align: center;
           margin-bottom: 100px !important;
-          padding: 0 1em;
+          padding: 0 4px;
         }
   </style>
 </head>
