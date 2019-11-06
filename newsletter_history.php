4cddee851601958c52b5311bdec7b904dd0aa2bc
Olatunji <sirhollyfat@gmail.com>
2019-11-06 14:03:00 +0100

    Fix newsletter archive page

--- newsletter_history.php
+++ newsletter_history.php
@@ -2,7 +2,57 @@
 require 'classControllers/init.php';
 // check if session is set
 if(!isset($_SESSION["role"])) {
-	header('Location:login.php'); 
+    header('Location:login.php');
+}
+
+
+if(isset($_POST['ok'])){
+
+    $subscriber = new Subscribers();
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $subscription_lists = $subscriber->fetch_list();
+
+    $email_lists = array();
+
+    foreach ($subscription_lists as $subscription_list){
+        $email_lists[] = $subscription_list['email'];
+    }
+
+
+    $file = $_FILES['image'];
+    $fileName = $file['name'];
+    $fileTmpName = $file['tmp_name'];
+    $fileSize = $file['size'];
+    $fileError = $file['error'];
+    $fileType = $file['type'];
+
+    $fileExtension = explode('.', $fileName);
+    $fileActualExtension = strtolower(end($fileExtension));
+
+    $allowed = array('jpg', 'jpeg', 'png');
+
+    if(in_array($fileActualExtension, $allowed)){
+
+        $fileNewName = uniqid('', true).".".$fileActualExtension;
+
+
+        $fileDestination = 'uploads/newsletter/'.$fileNewName;
+        move_uploaded_file($fileTmpName, $fileDestination);
+
+
+
+    }else{
+        $fileNewName = "";
+    }
+
+    $subscriber->saveNewsletter($subject, $message, $fileNewName);
+    send_general_email("$subject","no-reply@hng.tech",$message,$email_lists);
+
+    $_SESSION['msg'] = "<div class='alert alert-info'>New update sent successfully!</div>";
+    header("location:news_update.php");
+    exit();
 }
 
 
@@ -10,69 +60,72 @@ if(!isset($_SESSION["role"])) {
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8" />
+    <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-	<title>News Update Arhcive</title>
-	<link rel="icon" type="img/png" href="images/hng-favicon.png">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <title>News Update Archive</title>
+    <link rel="icon" type="img/png" href="images/hng-favicon.png">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link href="css/dashboard.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="css/newsletter.css">
+    <style type="text/css">
+        .card {
+            height: 150px;
+            background: #f3f3f3;
+            margin: 15px;
+            padding: 20px;
+            border-radius: 8px;
+            text-align: center;
+            font-size: 20px;
+            box-shadow: 1px 0 4px rgba(0,0,0,0.3);
 
+        }
 
-	<style type="text/css">
-		.card {
-			height: 150px;
-			background: #f3f3f3;
-			margin: 15px;
-			padding: 20px;
-			border-radius: 8px;
-			text-align: center;
-			font-size: 20px;
-			box-shadow: 1px 0 4px rgba(0,0,0,0.3);
-			
-		}
-
-		.card p{
-			font-weight: bolder;
-			font-size: 35px;
-		}
-	</style>
+        .card p{
+            font-weight: bolder;
+            font-size: 35px;
+        }
+    </style>
 
 </head>
 <body>
-	<main>
-		<section id="overview-section">
-			<h1>News Update Archive<span class="pull-right"></h1>
+<main>
+    <section id="overview-section">
+        <h1>News Update Archive</h1>
 
-            <hr>
+        <hr>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="row">
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
+    </section>
+</main>
 
+<input type="checkbox" id="mobile-bars-check" />
+<label for="mobile-bars-check" id="mobile-bars">
+    <div class="stix" id="stik1"></div>
+    <div class="stix" id="stik2"></div>
+    <div class="stix" id="stik3"></div>
+</label>
+<?php include('fragments/sidebar.php'); ?>
 
-		</section>
-	</main>
-
-	<input type="checkbox" id="mobile-bars-check" />
-    <label for="mobile-bars-check" id="mobile-bars">
-        <div class="stix" id="stik1"></div>
-        <div class="stix" id="stik2"></div>
-        <div class="stix" id="stik3"></div>
-    </label>
-	<?php include('fragments/sidebar.php'); ?>
-
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>tinymce.init({selector:'.textarea'});</script>
 </body>
 </html>
 <script  type="text/javascript" src="js/sidebar.js"></script>
