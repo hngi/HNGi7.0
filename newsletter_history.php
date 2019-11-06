b6f4724d4364dc16841a8d1784a4a46fb3773d2b
Olatunji <sirhollyfat@gmail.com>
2019-11-06 12:43:50 +0100

    Create newsletter archive page

---
+++ newsletter_history.php
@@ -0,0 +1,79 @@
+<?php
+require 'classControllers/init.php';
+// check if session is set
+if(!isset($_SESSION["role"])) {
+	header('Location:login.php'); 
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<title>News Update Arhcive</title>
+	<link rel="icon" type="img/png" href="images/hng-favicon.png">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="css/dashboard.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" />
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/newsletter.css">
+
+
+	<style type="text/css">
+		.card {
+			height: 150px;
+			background: #f3f3f3;
+			margin: 15px;
+			padding: 20px;
+			border-radius: 8px;
+			text-align: center;
+			font-size: 20px;
+			box-shadow: 1px 0 4px rgba(0,0,0,0.3);
+			
+		}
+
+		.card p{
+			font-weight: bolder;
+			font-size: 35px;
+		}
+	</style>
+
+</head>
+<body>
+	<main>
+		<section id="overview-section">
+			<h1>News Update Archive<span class="pull-right"></h1>
+
+            <hr>
+
+
+
+
+		</section>
+	</main>
+
+	<input type="checkbox" id="mobile-bars-check" />
+    <label for="mobile-bars-check" id="mobile-bars">
+        <div class="stix" id="stik1"></div>
+        <div class="stix" id="stik2"></div>
+        <div class="stix" id="stik3"></div>
+    </label>
+	<?php include('fragments/sidebar.php'); ?>
+
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>tinymce.init({selector:'.textarea'});</script>
+</body>
+</html>
+<script  type="text/javascript" src="js/sidebar.js"></script>
+<script type="text/javascript" src="js/dashboard.js"></script>
