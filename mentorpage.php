b36a26cbfb8fcf0250f668ea39ce84453d9c881f
Emmanuel Owojori <34447078+phiiileo@users.noreply.github.com>
2019-11-07 16:53:33 +0100

    Update mentorpage.php

--- mentorpage.php
+++ mentorpage.php
@@ -69,7 +69,7 @@ $mentors = $mentor->showMentors();
                 <div class="col-xl-3 col-sm-6 mb-3">
                     <div class="bg-white rounded  py-5 px-4"><img
                             src="<?= $ment['photo_url'] ;  ?>"
-                            alt="" width="200px" height="200px" class="img-fluid rounded-circle mb-3  ">
+                            alt="" width="100%" height="auto" class="img-fluid rounded-circle mb-3  ">
                         <h5 class="mb-2"><?= $ment['name']; ?></h5>
                         <span class="text-muted"><?= $ment['area_of_expertise'] ; ?></span>
                         <ul class="social mb-0 list-inline mt-5">
