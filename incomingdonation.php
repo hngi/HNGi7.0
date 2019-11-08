d19261507588a8d9c37daacc6f904a8c8e70bf19
hayjay-coder <lilianovrawah@gmail.com>
2019-11-08 21:18:25 +0100

    incoming donation page

--- incomingdonation.php
+++ incomingdonation.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Active Interns</title>
+    <title>Incoming Donation</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-    <link rel="stylesheet" type="text/css" href="css/activeinterns.css">
+    <link rel="stylesheet" type="text/css" href="css/incomingdonation.css">
     <link rel="stylesheet" type="text/css" href="css/header-footer.css">
     <link rel="icon" type="img/png" href="images/hng-favicon.png">
 </head>
 <body>
 <?php include('fragments/site_header.php');?>
 <div class ="container my-4">
-    <h2>ACTIVE INTERN </h2>
-    <p ><i class="fa fa-search"></i><input type="text"></p>
+    <h2>Incoming Donation</h2>
+    <!-- <p ><i class="fa fa-search"></i><input type="text"></p> -->
 <div class="container-scroll">
   
       <table>
@@ -44,39 +44,39 @@
             <td></td>
           </tr>
           <tr>
-            <td>3</td>
+            <td>4</td>
             <td></td>
             <td></td>
           </tr><tr>
-            <td>3</td>
+            <td>5</td>
             <td></td>
             <td></td>
           </tr><tr>
-            <td>3</td>
+            <td>6</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
-            <td>3</td>
+            <td>7<td>
             <td></td>
             <td></td>
           </tr>
           <tr>
-            <td>3</td>
+            <td>8</td>
             <td></td>
             <td></td>
           </tr><tr>
-            <td>3</td>
+            <td>9</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
-            <td>3</td>
+            <td>10</td>
             <td></td>
             <td></td>
           </tr>
           <tr>
-            <td>3</td>
+            <td>11</td>
             <td></td>
             <td></td>
           </tr>
