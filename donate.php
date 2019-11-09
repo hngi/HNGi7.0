6969e4287d4a5b81c31fde546841ffbf2366e85c
Olatunji <sirhollyfat@gmail.com>
2019-11-09 21:09:14 +0100

    Setup donation

--- donate.php
+++ donate.php
@@ -4,6 +4,71 @@
     $donation = new Donation();
 
     if(isset($_POST['pay'])){
+        $name = $database->escape_string($_POST['name']);
+        $phone = $database->escape_string($_POST['phone']);
+        $email = $database->escape_string($_POST['email']);
+        $amount = $database->escape_string($_POST['amount']);
+
+        $ref = $donation->pay("$amount","$name","$phone","$email");
+
+        $url = $_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST']."/verify_pay.php";
+
+
+        $curl = curl_init();
+
+        $customer_email = $email;
+//$amount = $amount;
+//$currency = "USD";
+        $txref = $ref; // ensure you generate unique references per transaction.
+        $PBFPubKey = "FLWPUBK_TEST-2c7dd09d63d44deae5cbf3e9728aa289-X"; // get your public key from the dashboard.
+        $redirect_url = $url;
+//$payment_plan = "pass the plan id"; // this is only required for recurring payments.
+
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => LIVE_POINT,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode(
+                array(
+                    'amount'=>$amount,
+                    'customer_email'=>$customer_email,
+                    'currency'=> "NGN",
+                    'txref'=>$txref,
+                    'PBFPubKey'=>$PBFPubKey,
+                    'redirect_url'=>$redirect_url
+                    //'payment_plan'=>$payment_plan
+                )
+            ),
+            CURLOPT_HTTPHEADER =>
+                array(
+                    "content-type: application/json",
+                    "cache-control: no-cache"
+                ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        if($err){
+            // there was an error contacting the rave API
+            die('Curl returned error: ' . $err);
+        }
+
+        $transaction = json_decode($response);
+
+        if(!$transaction->data && !$transaction->data->link){
+            // there was an error from the API
+            print_r('API returned error: ' . $transaction->message);
+        }
+
+// uncomment out this line if you want to redirect the user to the payment page
+//print_r($transaction->data->message);
+
+
+// redirect to page so User can pay
+// uncomment this line to allow the user redirect to the payment page
+        header('Location: ' . $transaction->data->link);
 
         exit();
     }
