fd9a444085a3bbf18db6c906e2b19ab94a7ba40d
Olatunji <sirhollyfat@gmail.com>
2019-11-09 20:58:09 +0100

    function to create payment reference

--- classControllers/Donate.php
+++ classControllers/Donate.php
@@ -9,4 +9,21 @@
 class Donate
 {
 
+
+    /**
+     * Donate constructor.
+     */
+    private $db;
+    public function __construct()
+    {
+        global $database;
+        $this->db = $database;
+    }
+
+    function pay($amount,$name,$phone,$email){ //function to create payment reference
+        $ref = rand(0,9).rand(0,9).rand(0,9).rand(0,9).rand(0,9);
+
+        $save = $this->db->query("INSERT INTO payments SET names = '$name', amount = '$amount', email = '$email', phone ='$phone', ref = '$ref'");
+        return $ref;
+    }
 }
