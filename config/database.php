f92c26e6d664487eca4b831b51aaeb8b453a8c87
Seyi Onifade <xyluz@ymail.com>
2020-05-14 06:04:38 +0100

    fix

--- config/database.php
+++ config/database.php
@@ -1,23 +1,36 @@
 <?php
 
 require_once "constants.php";
+require __DIR__ . '/../vendor/autoload.php';
 session_start();
 
 class DB
 {
 
     public $db;
-
+    public $host;
+    public $user;
+    public $pass;
+    public $dbname;
+    
     function __construct()
     {
 
         $this->get_connection();
+
+        $env = Dotenv\Dotenv::createImmutable(__DIR__);
+        $env->load();
+
+        $this->host=getenv('HOST');
+        $this->user=getenv('USERNAME');
+        $this->pass=getenv('PASSWORD');
+        $this->dbname=getenv('DBNAME'); 
     }
 
 
     public function get_connection()
     {
-        $this->db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $this->db = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
         if ($this->db->connect_errno) {
             die("Database connection failed" . $this->db->connect_error);
         }
