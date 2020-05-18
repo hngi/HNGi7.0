ec75d716bfa522d18e227a0e1694f5fceb396c16
Seyi Onifade <xyluz@ymail.com>
2020-05-18 04:34:15 +0100

    form adjustments

--- join-intern.php
+++ join-intern.php
@@ -11,14 +11,21 @@ if (isset($_POST['submit'])) {
     $email = $database->escape_string($_POST['email']);
     $phoneNo = $database->escape_string($_POST['phoneNo']);
     $linkCV = "blank";
-    //$interest = $database->escape_string($_POST['interest']);
+    $interest = $database->escape_string($_POST['interest']);
     $country = $database->escape_string($_POST["country"]);
-    $location = $database->escape_string($_POST['location']);
-    $empStatus = $database->escape_string($_POST['empStatus']);
+    $location = $database->escape_string($_POST['location']);    
     $about = $database->escape_string($_POST['about']);
     $date = $database->escape_string($_POST['date']);
-    //$interests=$database->escape_string($_POST['interest']);
+
+    $empStatus = $database->escape_string($_POST['empStatus']);
+    $professionalStatus = $database->escape_string($_POST['professional_status']);
+    $gender = $database->escape_string($_POST['gender']);
+    $incomeAmount = $database->escape_string($_POST['income_amount']);
+
+    
+
     $count = $intern->emailExists($email);
+
     if ($count === 0) {
         $res = $intern->internSignup();
         $request_mess = '<p style="text-align:center;">Application successful. Check your mail for registration message. Thank you!</a>';
@@ -127,7 +134,7 @@ if (!empty($request_mess)) {
                 <input type="text" name="phoneNo" id="phoneNo" required placeholder="Phone Number" />
 
                 <!-- Gender -->
-                <select id="gender" value="" class="gender" required>
+                <select id="gender" name="gender" value="" class="gender" required>
                     <option value="">Select Gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
