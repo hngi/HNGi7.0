6b41a4c0798297fea3fa2eb9ff1742a5d09c188d
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-05-18 13:40:32 +0100

    set limit on about input

--- join-intern.php
+++ join-intern.php
@@ -245,7 +245,7 @@ if (!empty($request_mess)) {
                 </select>
 
                 <!-- Brief Descriotion -->
-                <textarea name="about" id="about" required cols="30" rows="10"
+                <textarea name="about" id="about" maxlength="100" required cols="30" rows="2"
                     placeholder="Briefly tell us about yourself (not more than 100 words)"></textarea>
                 <input type='hidden' name='date' id="date" value='<?=date('Y-m-d H:i:s');?>'>
                 <p id="result"></p>
